7dfc91264af436665095fa1ab91c3adf31a7acd8
Grigory Latyshev <thegriglat@gmail.com>
2021-11-17 11:33:19 +0300

    rm

--- oracle.py
+++
@@ -1,7 +0,0 @@
-import cx_Oracle
-
-
-def OracleInit(user, password, host):
-    return cx_Oracle.connect(user=user, password=password,
-                             dsn=host,
-                             encoding="UTF-8")
